f668cb1731f1a4623cb9df123333a9d925d4e46a
therealrufus <77852621+therealrufus@users.noreply.github.com>
2022-03-21 14:45:50 +0100

    movement

--- jetpack/Assets/PlayerController.cs
+++ jetpack/Assets/PlayerController.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rb;
+    public float speed;
+    public float jetSpeed;
+    public int maxFuel = 300;
+    public int fuel = 300;
+    public float gravity = 10f;
+
+    Vector2 movement = new Vector2();
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY;
+        if (Input.GetKey(KeyCode.Space) && fuel > 0)
+        {
+            fuel--;
+            inputY = jetSpeed - gravity * Time.deltaTime;
+        }
+        else
+        {
+            inputY = -gravity*Time.deltaTime;
+            if (fuel < maxFuel)
+            {
+                fuel++;
+            }
+        }
+        movement = new Vector2(speed * inputX * Time.deltaTime, inputY);
+
+        rb.AddForce(movement);
     }
 }
