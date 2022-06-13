32b03f8ad4982911fca0bc4e41b85f954010b9de
Davi15S <nguyenxd072@gmail.com>
2022-06-13 14:05:26 +0200

    init

---
+++ jetpack/Assets/laserGenerator.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laserGenerator : MonoBehaviour
+{
+    public Animation animation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void LaserSpawn()
+    {
+
+    }
+}
